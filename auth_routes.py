5ae37b49544a28114869c7f4c46e7a7f981670aa
Marcelo Carneiro <marcelo_tech100@hotmail.com>
2026-02-22 13:59:31 -0300

    Create user account, schemas and cryptography

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Usuario
+from dependencies import pegar_sessao
+from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar() -> dict[str, str | bool]:
+async def home() -> dict[str, str | bool]:
     """
     Esta é a rota padrão de autenticação do nosso sistema.
     """
     return {"mensagem": "Você acessou a rota padrão de autenticação.", "autenticado": False}
+
+
+@auth_router.post("/criar_conta")
+async def criar_conta(usuario_schema: UsuarioSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
+
+    if usuario:
+        raise HTTPException(status_code=400, detail="E-mail de usuário já cadastrado.")
+    else:
+        senha_hash = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(nome=usuario_schema.nome, email=usuario_schema.email, senha=senha_hash, ativo=usuario_schema.ativo, admin=usuario_schema.admin)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": f"Usuário cadastrado com sucesso: {usuario_schema.email}"}
+   
